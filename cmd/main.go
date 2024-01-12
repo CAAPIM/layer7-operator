825c4a75bd3bf45afb12b79619875f0e54559116
kiran vaddadi <kirankumarnaidu@broadcom.com>
2024-01-12 16:38:16 +0530

    url from env

--- cmd/main.go
+++ cmd/main.go
@@ -210,12 +210,8 @@ func initProvider() (func(context.Context) error, func(context.Context) error, e
 	// probably connect directly to the service through dns.
 	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
-	// collectorURL = strings.Replace(collectorURL, "https://", "", 1)
-	// collectorURL = strings.Replace(collectorURL, "http://", "", 1)
 
-	setupLog.Info("Otel Collector url %s", collectorURL)
-
-	conn, err := grpc.DialContext(ctx, "localhost:4317",
+	conn, err := grpc.DialContext(ctx, collectorURL,
 		// Note the use of insecure transport here. TLS is recommended in production.
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithBlock(),

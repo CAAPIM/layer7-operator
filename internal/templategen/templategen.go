173bbb3b9884a37a7cfbf0d77be761e064beacdf
kiran vaddadi <kirankumarnaidu@broadcom.com>
2024-03-21 12:25:37 +0530

    remvove duplicate

--- internal/templategen/templategen.go
+++ internal/templategen/templategen.go
@@ -1,10 +1,10 @@
 package templategen
 
 type PortalAPI struct {
+	TenantId     string `json:"tenantId"`
 	Uuid         string `json:"apiUuid"`
 	UuidStripped string `json:"apiId"`     // templates have cases where uuid is stripped of -
 	ServiceId    string `json:"serviceId"` // Portal calculates this as UUID.nameUUIDFromBytes(api.getUuid().toString().getBytes());
-	TenantId     string `json:"tenantId"`
 	Name         string `json:"name"`
 	//Description       string           `json:"description"`
 	//Type              string           `json:"type"`

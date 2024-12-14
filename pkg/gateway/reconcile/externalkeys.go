a3e62f2b6099f657d5af17de04f9e089b78c3e72
Gary Vermeulen <garyvermeulen@live.com>
2024-12-14 09:21:59 +0000

    updated external keys sync

--- pkg/gateway/reconcile/externalkeys.go
+++ pkg/gateway/reconcile/externalkeys.go
@@ -1,3 +1,29 @@
+/*
+* Copyright (c) 2024 Broadcom. All rights reserved.
+* The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+* All trademarks, trade names, service marks, and logos referenced
+* herein belong to their respective companies.
+*
+* This software and all information contained therein is confidential
+* and proprietary and shall not be duplicated, used, disclosed or
+* disseminated in any way except as authorized by the applicable
+* license agreement, without the express written permission of Broadcom.
+* All authorized reproductions must be marked with this language.
+*
+* EXCEPT AS SET FORTH IN THE APPLICABLE LICENSE AGREEMENT, TO THE
+* EXTENT PERMITTED BY APPLICABLE LAW OR AS AGREED BY BROADCOM IN ITS
+* APPLICABLE LICENSE AGREEMENT, BROADCOM PROVIDES THIS DOCUMENTATION
+* "AS IS" WITHOUT WARRANTY OF ANY KIND, INCLUDING WITHOUT LIMITATION,
+* ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+* PURPOSE, OR. NONINFRINGEMENT. IN NO EVENT WILL BROADCOM BE LIABLE TO
+* THE END USER OR ANY THIRD PARTY FOR ANY LOSS OR DAMAGE, DIRECT OR
+* INDIRECT, FROM THE USE OF THIS DOCUMENTATION, INCLUDING WITHOUT LIMITATION,
+* LOST PROFITS, LOST INVESTMENT, BUSINESS INTERRUPTION, GOODWILL, OR
+* LOST DATA, EVEN IF BROADCOM IS EXPRESSLY ADVISED IN ADVANCE OF THE
+* POSSIBILITY OF SUCH LOSS OR DAMAGE.
+*
+ */
+
 package reconcile
 
 import (
@@ -14,13 +40,8 @@ import (
 
 func ExternalKeys(ctx context.Context, params Params) error {
 	gateway := params.Instance
-	if len(gateway.Spec.App.ExternalKeys) == 0 {
-		for _, v := range gateway.Status.LastAppliedExternalKeys {
-			if len(v) != 0 {
-				continue
-			}
-			return nil
-		}
+	if len(gateway.Spec.App.ExternalKeys) == 0 && len(gateway.Status.LastAppliedExternalKeys) == 0 {
+		return nil
 	}
 
 	name := gateway.Name
@@ -46,40 +67,23 @@ func ExternalKeys(ctx context.Context, params Params) error {
 		return err
 	}
 
+	notFound := []string{}
+	keySecretMap := []util.GraphmanKey{}
 	for _, k := range gateway.Status.LastAppliedExternalKeys {
 		found := false
-		notFound := []string{}
-
 		for _, ek := range gateway.Spec.App.ExternalKeys {
-			if k == ek.Alias {
+			if k == ek.Alias && ek.Enabled {
 				found = true
 			}
 		}
 		if !found {
 			notFound = append(notFound, k)
-			bundleBytes, err := util.ConvertX509ToGraphmanBundle(nil, notFound)
-			if err != nil {
-				return err
-			}
-
-			annotation := "security.brcmlabs.com/external-key-" + k
-			if !gateway.Spec.App.Management.Database.Enabled {
-				err = ReconcileEphemeralGateway(ctx, params, "external keys", *podList, gateway, gwSecret, "", annotation, "deleted", false, k, bundleBytes)
-				if err != nil {
-					return err
-				}
-			} else {
-				err = ReconcileDBGateway(ctx, params, "external keys", gatewayDeployment, gateway, gwSecret, "", annotation, "deleted", false, k, bundleBytes)
-				if err != nil {
-					return err
-				}
-			}
 		}
 	}
 
+	var sha1Sum string
 	for _, externalKey := range gateway.Spec.App.ExternalKeys {
-		var sha1Sum string
-		keySecretMap := []util.GraphmanKey{}
+
 		if externalKey.Enabled {
 			secret, err := getGatewaySecret(ctx, params, externalKey.Name)
 			if err != nil {
@@ -101,53 +105,34 @@ func ExternalKeys(ctx context.Context, params Params) error {
 					UsageType: usageType,
 				})
 			}
-
-			dataBytes, _ := json.Marshal(&secret.Data)
-			h := sha1.New()
-			h.Write(dataBytes)
-			sha1Sum = fmt.Sprintf("%x", h.Sum(nil))
 		}
+	}
 
-		notFound := []string{}
-		if gateway.Status.LastAppliedExternalKeys != nil {
-			for _, appliedKey := range gateway.Status.LastAppliedExternalKeys {
-				found := false
-				for _, desiredKey := range keySecretMap {
-					if appliedKey == desiredKey.Alias {
-						found = true
-					}
-				}
-				if !found {
-					notFound = append(notFound, appliedKey)
-				}
-			}
-		}
+	if len(keySecretMap) < 1 && len(notFound) < 1 {
+		return nil
+	}
 
-		if len(keySecretMap) < 1 && len(notFound) < 1 {
-			continue
-		}
+	bundleBytes, err := util.ConvertX509ToGraphmanBundle(keySecretMap, notFound)
+	if err != nil {
+		return err
+	}
 
-		bundleBytes, err := util.ConvertX509ToGraphmanBundle(keySecretMap, notFound)
+	dataBytes, _ := json.Marshal(&keySecretMap)
+	h := sha1.New()
+	h.Write(dataBytes)
+	sha1Sum = fmt.Sprintf("%x", h.Sum(nil))
+
+	annotation := "security.brcmlabs.com/external-keys"
+
+	if !gateway.Spec.App.Management.Database.Enabled {
+		err = ReconcileEphemeralGateway(ctx, params, "external keys", *podList, gateway, gwSecret, "", annotation, sha1Sum, false, "", bundleBytes)
 		if err != nil {
 			return err
 		}
-
-		if sha1Sum == "" {
-			sha1Sum = "deleted"
-		}
-
-		annotation := "security.brcmlabs.com/external-key-" + externalKey.Alias
-
-		if !gateway.Spec.App.Management.Database.Enabled {
-			err = ReconcileEphemeralGateway(ctx, params, "external keys", *podList, gateway, gwSecret, "", annotation, sha1Sum, false, externalKey.Name, bundleBytes)
-			if err != nil {
-				return err
-			}
-		} else {
-			err = ReconcileDBGateway(ctx, params, "external keys", gatewayDeployment, gateway, gwSecret, "", annotation, sha1Sum, false, externalKey.Name, bundleBytes)
-			if err != nil {
-				return err
-			}
+	} else {
+		err = ReconcileDBGateway(ctx, params, "external keys", gatewayDeployment, gateway, gwSecret, "", annotation, sha1Sum, false, "", bundleBytes)
+		if err != nil {
+			return err
 		}
 	}
 	return nil

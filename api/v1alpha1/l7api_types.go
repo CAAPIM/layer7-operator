ded1a2765def184b9bcad8fa7b6cf04faa26ef70
Tony Guo <102392106+tg892580@users.noreply.github.com>
2025-04-02 16:19:49 -0700

    US1000588 api deployment error handling (#65)
    * US1000588 add conditions array under status.gateways in l7api

--- api/v1alpha1/l7api_types.go
+++ api/v1alpha1/l7api_types.go
@@ -1,18 +1,4 @@
-/*
-Copyright 2021.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright (c) 2025 Broadcom Inc. and its subsidiaries. All Rights Reserved.
 
 package v1alpha1
 
@@ -108,14 +94,20 @@ type CustomField struct {
 	Value string `json:"value"`
 }
 
+type GatewayPodDeploymentCondition struct {
+	Action     string `json:"action,omitempty"`
+	ActionTime string `json:"actionTime,omitempty"`
+	Checksum   string `json:"checksum,omitempty"`
+	Status     string `json:"status,omitempty"`
+	Reason     string `json:"reason,omitempty"`
+}
+
 type LinkedGatewayStatus struct {
 	Name string `json:"name,omitempty"`
 	//Phase       corev1.PodPhase `json:"phase,omitempty"`
-	// Reason: Success/Failed to sync because of x
 	Deployment string `json:"deployment,omitempty"`
 	//Ready       bool            `json:"ready,omitempty"`
-	LastUpdated string `json:"lastUpdated,omitempty"`
-	Checksum    string `json:"checksum,omitempty"`
+	Conditions []GatewayPodDeploymentCondition `json:"conditions,omitempty"`
 }
 
 func init() {
